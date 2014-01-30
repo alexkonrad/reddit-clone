2ca39c3874dc2d91fac7b99921c535fd8dc5d296
Alex Konrad <alexkonrad08@gmail.com>
2014-01-30 17:16:03 -0500

    delete comment

--- app/models/user_vote.rb
+++ app/models/user_vote.rb
@@ -1,7 +1,6 @@
 class UserVote < ActiveRecord::Base
   attr_accessible :link_id, :user_id, :vote
 
-  # validates_uniqueness_of :user_id, scope: :link_id
   validates :user_id, uniqueness: { scope: :link_id }
   validates :vote, inclusion: { in: %[up down] }
 
